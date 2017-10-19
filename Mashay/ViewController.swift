b13897cf0dd432ff46161a1eb6ac0178a4b99d9e
Ali Faris <alihcompiler@gmail.com>
2017-10-19 08:49:35 +0300

    get today step count and total step count

--- Mashay/ViewController.swift
+++ Mashay/ViewController.swift
@@ -12,43 +12,73 @@ import CoreMotion
 class ViewController: UIViewController
 {
     
-    @IBOutlet weak var steps: UILabel!
+    let FIRST_TIME_DATE = "FIRST_TIME_DATE";
+    
+    @IBOutlet weak var todaySteps: UILabel!
+    @IBOutlet weak var totalSteps: UILabel!
+
     
     let pedoMeter = CMPedometer()
     
     override func viewDidLoad()
     {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        
+        saveFirstTimeDateIfNot();
+        readPedometerData();
+    }
+    
+    
+    func readPedometerData()
+    {
         if(CMPedometer.isStepCountingAvailable())
         {
-            let fromDate = Date(timeIntervalSinceNow: -86400 * 7);
+            let time = UserDefaults.standard.object(forKey: FIRST_TIME_DATE) as! Int;
+            let interval = TimeInterval(time);
+            let fromDate = Date(timeIntervalSinceNow: interval);
+            print(fromDate);
             self.pedoMeter.queryPedometerData(from: fromDate, to: Date())
             {
                 (data : CMPedometerData!, error) -> Void in
                 DispatchQueue.main.async(execute:
-                {
-                    () -> Void in
-                    
-                    if(error == nil){
-                        self.steps.text = "\(data.numberOfSteps)"
-                    }
+                    {
+                        () -> Void in
+                        if(error == nil){
+                            self.totalSteps.text = "\(data.numberOfSteps)"
+                        }
                 })
             }
             
-            self.pedoMeter.startUpdates(from: Date(timeIntervalSinceNow: -86400 * 7))
+            self.pedoMeter.startUpdates(from: Date())
             {
                 (data: CMPedometerData!, error) -> Void in
                 DispatchQueue.main.async(execute:
-                {
-                    () -> Void in
-                    
-                    if(error == nil){
-                        self.steps.text = "\(data.numberOfSteps)"
-                    }
+                    {
+                        () -> Void in
+                        if(error == nil){
+                            self.todaySteps.text = "\(data.numberOfSteps)"
+                        }
                 })
             }
         }
+        else
+        {
+            self.todaySteps.text = "غير معروف"
+            self.totalSteps.text = "غير معروف"
+        }
     }
+    
+    func saveFirstTimeDateIfNot()
+    {
+        let firstTimeDate = UserDefaults.standard.object(forKey: FIRST_TIME_DATE);
+        if firstTimeDate == nil
+        {
+            let date = Date();
+            UserDefaults.standard.set(date.timeIntervalSince1970, forKey: FIRST_TIME_DATE)
+            UserDefaults.standard.synchronize();
+        }
+    }
+    
 }
 
 
